b9b86e130f89c35e0076cfe9188c305cb4a9a63b
alexg <105132716+AlexGundrum@users.noreply.github.com>
2025-12-22 12:56:18 -0600

    Time: 808 ms (15.93%) | Memory: 212.9 MB (13.02%) - LeetSync

---
+++ solutions/Python_Solutions/712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.py
@@ -0,0 +1,48 @@
+'''
+ok so we delete stuff and have to make the strings equal
+
+we want ascii sum to be minijmized
+
+dp(i,j):
+min cost to make s1[:i] == s2[:j]
+
+base cases: 
+if both are in bounds and ==, just go to dp i + 1, j + 1
+
+if both are oob return 0
+
+if one is oob and other isn't we need to delete the rest of the in bounds. 
+
+if both are in bounds and not equal, try deleting each and return min. 
+
+'''
+from functools import lru_cache
+
+class Solution:
+    def minimumDeleteSum(self, s1: str, s2: str) -> int:
+        
+        @lru_cache(maxsize = None)
+        def dp(i, j):
+            if (i >= len(s1) and j >= len(s2)):
+                return 0 #both oob
+            
+            if (i >= len(s1)):
+                #i is oob
+                return ord(s2[j]) + dp(i, j + 1)
+
+            if (j >= len(s2)):
+                #j is oob
+                return ord(s1[i]) + dp(i + 1, j)
+            
+            #both are in bounds. 
+            if s1[i] == s2[j]:
+                #equal, no deletes needed
+                return dp(i + 1, j +1)
+            
+            leftDel = ord(s1[i]) + dp(i + 1, j)
+            rightDel = ord(s2[j]) + dp(i, j + 1)
+
+            return min(leftDel, rightDel)
+
+        return dp(0,0)
+    
